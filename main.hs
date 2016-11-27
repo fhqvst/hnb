8573c42d989b6549bcedf40b0450c40dfd401eb8
Filip Hallqvist <filiphallqvist@gmail.com>
2016-11-27 23:49:12 +0100

    Added hotfix for Lazy IO which apparently does not work

--- main.hs
+++ main.hs
@@ -1,10 +1,12 @@
 module Main where
 
+import System.IO
 import Data.List
 import Data.List.Split
 import Data.Char
 import Text.Printf
-import qualified Data.Map.Strict as M
+import qualified Data.Map.Strict as M 
+import Control.DeepSeq
 
 data Sentiment = Pos | Neg
   deriving Show
@@ -12,51 +14,54 @@ data Sentiment = Pos | Neg
 tokenize :: String -> [String]
 tokenize string = filter (not . null) $ splitOn " " [ toLower char | char <- string, char `notElem` ",.?!-:;\"\'" ]
 
-positive = [("This is some awesome text", Pos),("The text is really good", Pos),("The food was great!", Pos)]
-
-negative = [("What an awful text this is", Neg),("I absolutely hate these words", Neg),("The food was bad! I hated every second", Neg)]
--- Tokenize sentences
-tokenizedPos = map (\(t,s) -> (tokenize t, s)) positive 
-tokenizedNeg = map (\(t,s) -> (tokenize t, s)) negative
-
--- Count words
-posWords = concatMap fst tokenizedPos
-negWords = concatMap fst tokenizedNeg 
-posCount = M.fromListWith (+) [ (word, 1.0) | word <- posWords ]
-negCount = M.fromListWith (+) [ (word, 1.0) | word <- negWords ]
-
--- Split into training vs. testing
-splitPos = splitAt (floor $ 0.75 * fromIntegral (length tokenizedPos)) tokenizedPos
-splitNeg = splitAt (floor $ 0.75 * fromIntegral (length tokenizedNeg)) tokenizedNeg
-
-trainPos = fst splitPos
-trainNeg = fst splitNeg
-
-testPos = snd splitPos
-testNeg = snd splitNeg
-
--- Calculate prior probabilities
-totalCount = fromIntegral $ length (posWords ++ negWords)
-priorPos = sum (M.elems posCount) / totalCount
-priorNeg = sum (M.elems negCount) / totalCount
+parseData :: FilePath -> IO String
+parseData fileName = withFile fileName ReadMode $
+  \h -> do hSetEncoding h utf8_bom
+           contents <- hGetContents h
+           contents `deepseq` hClose h
+           return contents
 
 probWord :: String -> M.Map String Double -> Double
 probWord string corpus = (M.findWithDefault 1.0 string corpus) / fromIntegral (length (M.keys corpus))
 
-classifyPos :: String -> Double
-classifyPos string = priorPos * product probWords
-  where probWords = map (\word -> probWord word posCount) (tokenize string)
-
-classifyNeg :: String -> Double
-classifyNeg string = priorNeg * product probWords
-  where probWords = map (\word -> probWord word negCount) (tokenize string)
+classify :: String -> M.Map String Double -> Double -> Double
+classify string corpus prior = prior * product probWords
+  where probWords = map (\word -> probWord word corpus) (tokenize string)
 
 main :: IO ()
 main = do
+ 
+  corpusPos <- parseData "data/positive.txt"
+  corpusNeg <- parseData "data/negative.txt"
+
+  let tokenizedPos = seq corpusPos [ (tokenize string, Pos) | string <- lines corpusPos ]
+  let tokenizedNeg = seq corpusNeg [ (tokenize string, Pos) | string <- lines corpusNeg ]
+
+  -- Count words
+  let posWords = concatMap fst tokenizedPos
+  let negWords = concatMap fst tokenizedNeg
+  let posCount = M.fromListWith (+) [ (word, 1.0) | word <- posWords ]
+  let negCount = M.fromListWith (+) [ (word, 1.0) | word <- negWords ]
+  
+  -- Split into training vs. testing
+  let splitPos = splitAt (floor $ 0.75 * fromIntegral (length tokenizedPos)) tokenizedPos
+  let splitNeg = splitAt (floor $ 0.75 * fromIntegral (length tokenizedNeg)) tokenizedNeg
+
+  let trainPos = fst splitPos
+  let trainNeg = fst splitNeg
+
+  let testPos = snd splitPos
+  let testNeg = snd splitNeg
+
+  -- Calculate prior probabilities
+  let totalCount = fromIntegral $ length (posWords ++ negWords)
+  let priorPos = sum (M.elems posCount) / totalCount
+  let priorNeg = sum (M.elems negCount) / totalCount
+
   -- Classify a test string
   let testString = "Absolutely, I hate these... words."  
 
-  putStrLn $ printf "Positive: %.9f" $ classifyPos testString
-  putStrLn $ printf "Negative: %.9f" $ classifyNeg testString
+  putStrLn $ printf "Positive: %.9f" $ classify testString posCount priorPos
+  putStrLn $ printf "Negative: %.9f" $ classify testString negCount priorNeg
   
   putStrLn "Finished" 
